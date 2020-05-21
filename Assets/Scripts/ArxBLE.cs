c35a81ab9ace45b1a35939be3b97952400c571b4
Jaapdedood <jaapdd@gmail.com>
2020-05-21 14:53:47 -0700

    Initial Commit

---
+++ Assets/Scripts/ArxBLE.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ArxBLE : MonoBehaviour
+{
+    public GameObject Panel;
+    public Text msg;
+
+    public const string ServiceUUID = "0000e0ff-3c17-d293-8e48-14fe2e4da212";
+    public const string writeUUID = "0000ffe1-0000-1000-8000-00805f9b34fb";
+    public const string notifyUUID = "0000ffe2-0000-1000-8000-00805f9b34fb";
+
+	enum States
+	{
+		None,
+		Scan,
+		ScanRSSI,
+		Connect,
+		Subscribe,
+		Unsubscribe,
+		Disconnect,
+	}
+
+    private bool _connected = false;
+	private float _timeout = 0f;
+	private States _state = States.None;
+	private string _deviceAddress;
+	private bool _foundSubscribeID = false;
+	private bool _foundWriteID = false;
+	private byte[] _dataBytes = null;
+	private bool _rssiOnly = false;
+	private int _rssi = 0;
+
+    void Reset ()
+	{
+	   	_connected = false;
+	   	_timeout = 0f;
+        _state = States.None;
+	    _deviceAddress = null;
+	    _foundSubscribeID = false;
+    	_foundWriteID = false;
+	   	_dataBytes = null;
+	    _rssi = 0;
+	}
+
+	void SetState (States newState, float timeout)
+	{
+		_state = newState;
+		_timeout = timeout;
+	}
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        Reset ();
+		//Initialize(as central, as peripheral, Action, error Action)
+		BluetoothLEHardwareInterface.Initialize (true, false, () => {
+			
+			SetState (States.Scan, 0.1f);
+			
+		}, (error) => {
+			
+			BluetoothLEHardwareInterface.Log ("Error during initialize: " + error);
+		});
+    }
+
+    public void scanButton()
+    {
+        Panel.SetActive(true);
+
+        msg.text = "Starting Scan\n";   
+    }
+
+    void Update()
+    {
+        if(_timeout > 0f)
+        {
+            _timeout -= Time.deltaTime;
+            if(_timeout <= 0f)
+            {
+                _timeout = 0f;
+
+                switch(_state)
+                {
+                case States.None:
+                    break;
+                case States.Scan:
+                    break;
+                case  States.ScanRSSI:
+                    break;
+                case States.Connect:
+                    break;
+                case States.Subscribe:
+                    break;
+                case States.Unsubscribe:
+                    break;
+                case States.Disconnect:
+                    break;
+                }
+            }
+        }
+    }
+
+}
