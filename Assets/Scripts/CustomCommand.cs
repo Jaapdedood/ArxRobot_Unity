737b11ffc8ba458f44a446644fe48e59bcf5418c
Jaapdedood <jaapdd@gmail.com>
2020-06-15 23:36:08 -0700

    Buttons are cool just need to save them

---
+++ Assets/Scripts/CustomCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCommand : MonoBehaviour
+{
+    public int CommandID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CommandID = 0;
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    public void ButtonClick()
+    {
+        Debug.Log(CommandID);
+    }
+}
---
+++ Assets/Scripts/CustomCommandDisplay.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomCommandDisplay : MonoBehaviour
+{
+    public Transform Canvas;
+    public Button CustomButtonPrefab;
+    public string TempCommandString;
+    private Button _customButton;
+    private int _tempID;
+    private string _tempLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawner(string type)
+    {
+        //toggle off current panel
+        //currentPanel.SetActive(!currentPanel.activeSelf);
+
+        switch(type)
+        {
+            case "button":
+            	_customButton = (Button)Instantiate(CustomButtonPrefab);
+                _customButton.transform.SetParent(Canvas, false);
+
+                CustomCommand customCommandScript = _customButton.GetComponent<CustomCommand>();
+
+                _customButton.GetComponentInChildren<Button>().onClick.AddListener(customCommandScript.ButtonClick);
+                Debug.Log("Setting commandscript tempID:");
+                Debug.Log(_tempID);
+                customCommandScript.CommandID = _tempID;
+                _customButton.GetComponentInChildren<Text>().text = _tempLabel;
+            break;
+            case "toggle":
+            break;
+            case "slider":
+            break;
+            case "text":
+            break;
+        }
+    }
+    //Unity Inspector only allows string
+    public void CommandIDEntered(string value)
+    {
+        _tempID = Convert.ToInt32(value);
+    }
+
+    public void CommandLabelEntered(string label)
+    {
+        _tempLabel = label;
+    }
+}
